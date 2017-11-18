ac133e7a83b0f6720d64ff3ed2424e20dbdf37a9
René <rene@Renes-iMac.fritz.box>
2017-11-18 15:01:29 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-import { CreateEntryPageModule } from '../pages/create-entry/create-entry.module';
-
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { EntrystorageProvider } from '../providers/entrystorage/entrystorage';
-
-import { SecureStorage} from '@ionic-native/secure-storage';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage,
+    HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
-    CreateEntryPageModule
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+    HomePage
   ],
   providers: [
-    SecureStorage,
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    EntrystorageProvider
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,23 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {EntrystorageProvider} from '../../providers/entrystorage/entrystorage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  private entries = "hello";
-  constructor(public navCtrl: NavController, private entryProv: EntrystorageProvider) {
+
+  constructor(public navCtrl: NavController) {
 
   }
-  toggle () {
-    this.entryProv.set('12345', 'test').then((res) => {
-      console.log(res);
-    });
-    this.entryProv.entries().then((res) => {
-      this.entries = JSON.stringify(res);
-      console.log(res);
-    });
-  }
+
 }
